76e5fd9f20164006da42d99136e9d717f9afcda6
SteveJops <steven07kolesiko@gmail.com>
2021-01-06 16:07:33 +0200

    added shop views and urls with templates

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'shop/home.html')
+
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'shop/about.html')
