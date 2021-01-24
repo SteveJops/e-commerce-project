38993ed29cb35befbdea1fd7cb6305240a6de78d
SteveJops <steven07kolesiko@gmail.com>
2021-01-24 17:24:08 +0200

    editing views adding models and news templates of cart

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomeView, ProductView, CartView
+from .views import HomeView, ProductView, cart_detail, add_cart
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('category/<slug:category_slug>', HomeView.as_view(), name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', ProductView.as_view(), name='product_detail'),
-    path('cart/', CartView.as_view(), name='cart'),
+    path('cart/', cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', add_cart, name='add_cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.base import View
-from .models import Category, Product
+
+from .models import Category, Product, Cart, CartItem
 
 
 class HomeView(View):
@@ -23,6 +25,39 @@ class ProductView(View):
         return render(request, 'shop/product.html', {'product': product})
 
 
-class CartView(View):
-    def get(self, request):
-        return render(request, 'shop/cart.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+        cart_item.save()
+    return redirect('cart_detail')
+
+
+def cart_detail(request, cart_items=None, total=0,
+                counter=0):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            counter += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+
+    return render(request, 'shop/cart.html', dict(cart_items=cart_items, total=total, counter=counter))
