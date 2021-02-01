34f6ff42807a8ead3791a71ccc9758b94dc1a1c6
SteveJops <steven07kolesiko@gmail.com>
2021-02-01 17:34:46 +0200

    adding reduce items in cart button and remove all goods in the cart menu

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, ProductView, cart_detail, add_cart
+from .views import HomeView, ProductView, cart_detail, add_cart, cart_remove, cart_delete
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('category/<slug:category_slug>/<slug:product_slug>', ProductView.as_view(), name='product_detail'),
     path('cart/', cart_detail, name='cart_detail'),
     path('cart/add/<int:product_id>', add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', cart_remove, name='cart_remove'),
+    path('cart/delete/<int:product_id>', cart_delete, name='cart_delete'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -36,12 +36,14 @@ def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
+
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
         cart.save()
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
+        if cart_item.quantity < cart_item.product.stock:
+            cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
@@ -61,3 +63,23 @@ def cart_detail(request, cart_items=None, total=0,
         pass
 
     return render(request, 'shop/cart.html', dict(cart_items=cart_items, total=total, counter=counter))
+
+
+def cart_remove(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart_detail')
+
+
+def cart_delete(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart_detail')
